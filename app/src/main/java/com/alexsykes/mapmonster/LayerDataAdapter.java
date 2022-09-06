e440deb8ae6f2cb1c9de51f34a3131c0b4caef2e
Alex Sykes <alex@alexsykes.net>
2022-09-06 18:33:37 +0100

    WiP

--- app/src/main/java/com/alexsykes/mapmonster/LayerDataAdapter.java
+++ app/src/main/java/com/alexsykes/mapmonster/LayerDataAdapter.java
@@ -1,18 +1,17 @@
 package com.alexsykes.mapmonster;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.alexsykes.mapmonster.data.Layer;
 import com.alexsykes.mapmonster.data.LayerDao;
-import com.alexsykes.mapmonster.LayerDataViewHolder;
 
 import java.util.List;
 
-public class LayerDataAdapter extends RecyclerView.Adapter<LayerDataViewHolder> {
+public class LayerDataAdapter extends RecyclerView.Adapter<LayerDataAdapter.LayerDataViewHolder> {
     List<LayerDao.LayerData> layerData;
     public LayerDataAdapter(List<LayerDao.LayerData> allLayers) {
         this.layerData = allLayers;
@@ -30,8 +29,17 @@ public class LayerDataAdapter extends RecyclerView.Adapter<LayerDataViewHolder> 
 
     }
 
+
     @Override
     public int getItemCount() {
         return layerData.size();
+    }
+
+    public class LayerDataViewHolder extends RecyclerView.ViewHolder {
+        TextView layerNameTextView;
+        public LayerDataViewHolder(@NonNull View itemView) {
+            super(itemView);
+            layerNameTextView = itemView.findViewById(R.id.layerNameTextView);
+        }
     }
 }
