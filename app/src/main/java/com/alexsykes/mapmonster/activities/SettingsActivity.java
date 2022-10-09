8528b5a5dea747194b7b48abee24af1fbaeb6bbb
Alex Sykes <alex@alexsykes.net>
2022-10-09 20:55:32 +0300

    Marker CSV export - working

--- app/src/main/java/com/alexsykes/mapmonster/activities/SettingsActivity.java
+++ app/src/main/java/com/alexsykes/mapmonster/activities/SettingsActivity.java
@@ -5,12 +5,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -27,12 +24,10 @@ import com.alexsykes.mapmonster.data.LayerViewModel;
 import com.alexsykes.mapmonster.data.MMDatabase;
 import com.alexsykes.mapmonster.data.MarkerViewModel;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.List;
 
 import com.opencsv.*;
@@ -81,20 +76,21 @@ public class SettingsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.email_menu_item:
-                dbExport();
+                csvEmail();
                 return true;
 
             case R.id.export_menu_item:
-               fileDemo();
-               dbExport();
-//                exportData();
+               csvExport();
                 return true;
             default:
         }
         return false;
     }
 
-    private void fileDemo() {
+    private void csvEmail() {
+    }
+
+    private void csvExport() {
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.setType("text/csv");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -109,88 +105,31 @@ public class SettingsActivity extends AppCompatActivity {
         switch (requestCode) {
             case PICKFILE_RESULT_CODE:
                 if (resultCode == -1) {
-                    fileUri = data.getData();  // long form including content://com.android.externalstorage.documents/document/primary%3ADocuments%2Fdata%20(2).csv
-                    filePath = fileUri.getPath();  // String - /document/primary:Documents/data (2).csv
-
-                    Log.i(TAG, "fileUri: "  + fileUri);
-                    Log.i(TAG, "filePath: "  + filePath);
+                    fileUri = data.getData();
+                    filePath = fileUri.getPath();
                 }
 
-                File outfile = new File(filePath);
                 try {
-                    Cursor exportData = getExportData();
-                    FileWriter fileWriter = new FileWriter(filePath);
-                    CSVWriter csvWrite = new CSVWriter(fileWriter);
-
-                    csvWrite.writeNext(exportData.getColumnNames());
+                    OutputStream os = getContentResolver().openOutputStream(data.getData());
+                    Writer writer = new OutputStreamWriter(os);
+                    csvWriter = new CSVWriter(writer);
+                    Cursor exportData = getMarkersForExport();
                     while (exportData.moveToNext()) {
                         String arrStr[] = new String[exportData.getColumnCount()];
                         for (int i = 0; i < exportData.getColumnCount(); i++)
                             arrStr[i] = exportData.getString(i);
-                        csvWrite.writeNext(arrStr);
+                        csvWriter.writeNext(arrStr);
                     }
-                    csvWrite.close();
-                    exportData.close();
 
-                    Log.i(TAG, "Success");
-                } catch (Exception sqlEx) {
-                    Log.e(TAG, sqlEx.getMessage(), sqlEx);
+                    csvWriter.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
                 break;
         }
     }
 
-    private void exportData() {
-        Cursor exportData = getExportData();
-
-        File path = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOCUMENTS);
-        File file = new File(path, "Waypoints.csv");
-        try {
-            CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
-
-            csvWrite.writeNext(exportData.getColumnNames());
-            while (exportData.moveToNext()) {
-                String arrStr[] = new String[exportData.getColumnCount()];
-                for (int i = 0; i < exportData.getColumnCount(); i++)
-                    arrStr[i] = exportData.getString(i);
-                csvWrite.writeNext(arrStr);
-            }
-            csvWrite.close();
-            exportData.close();
-
-            Log.i(TAG, "Success");
-        } catch (Exception sqlEx) {
-            Log.e(TAG, sqlEx.getMessage(), sqlEx);
-        }
-    }
-
-    private void dbExport() {
-//        Cursor exportData = getExportData();
-
-
-            String DatabaseName = "mm_database";
-            File externalStorageDirectory = Environment.getExternalStorageDirectory();
-            File dataDirectory = Environment.getDataDirectory();
-            FileChannel source=null;
-            FileChannel destination=null;
-            String currentDBPath = "/data/"+ getPackageName() +"/databases/"+DatabaseName ;
-            String backupDBPath =  "Documents/back.db";
-            File currentDB = new File(dataDirectory, currentDBPath);
-            File backupDB = new File(externalStorageDirectory, backupDBPath);
-            try {
-                source = new FileInputStream(currentDB).getChannel();
-                destination = new FileOutputStream(backupDB).getChannel();
-                destination.transferFrom(source, 0, source.size());
-                source.close();
-                destination.close();
-                Toast.makeText(this, "Your Database is Exported !!", Toast.LENGTH_LONG).show();
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-    }
-
-    private Cursor getExportData() {
+    private Cursor getMarkersForExport() {
         MMDatabase db = MMDatabase.getDatabase(this);
         markerViewModel = new ViewModelProvider(this).get(MarkerViewModel.class);
         return markerViewModel.getMarkerDataForExport();
